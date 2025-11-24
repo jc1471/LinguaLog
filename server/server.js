ad2b81baa0afc2953dcaef7d0e9e9f36df8bd99c
jc1471 <jordanbcrow@gmail.com>
2025-11-24 20:25:18 +0100

    Troubleshoot Render deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 // Import packages
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const itemRoutes = require('./routes/itemRoutes');
@@ -9,33 +8,52 @@ const itemRoutes = require('./routes/itemRoutes');
 
 const app = express();
 // const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Enable CORS so React can make requests from front end to back end
+const allowedOrigins = [
+    'http://localhost:5173', // local dev
+    'https://lingualog-frontend.onrender.com' // deployed frontend
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: function (origin, callback) {
+        // Allows requests with no origin
+        if (!origin) return callback(null, true);
+
+        // allow only origins in our allowed list
+        if (allowedOrigins.includes(origin)) return callback(null, true);
+
+        // block everything else
+        return callback(new Error('CORS policy blocked this origin'));
+    },
     credentials: true
 }));
 
 // Reference API Routes
 app.use('/api/items', itemRoutes);
 
+/*
 // Connect to MongoDB
 //mongoose.connect(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error("MongoDB connection error", err));
+*/
 
 // Test route
 app.get('/api/test', (req, res) => {
     res.json({ message: 'Backend is talking :)' });
 });
 
-// Start server
+
+
+// Connect to mongodb
 connectDB();
+// Start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
